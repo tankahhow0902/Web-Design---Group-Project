671ddfcf677d8b1310464cda915c70208f05bd5d
chooyuelung0728 <chooyuelung0728@e.newera.edu.my>
2026-07-29 14:12:44 +0800

    Add files via upload

--- aboutUs.js
+++ aboutUs.js
@@ -21,11 +21,11 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "grid";
   dots[slideIndex-1].className += " active";
 }
 
 // 自动播放
-setInterval(() => {
-  plusSlides(1);
-}, 10000);
+//setInterval(() => {
+//  plusSlides(1);
+//}, 10000);
