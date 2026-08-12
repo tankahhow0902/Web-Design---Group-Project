57ac670065967939e32f84d10d7ad4e8477c9c1c
wangkahui0126-lgtm <wangkahui0126@e.newera.edu.my>
2026-08-12 14:47:26 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -13,3 +13,25 @@ cartButtons.forEach(function(button) {
     });
 
 });
+
+window.addEventListener("load", function () {
+    const navigation = performance.getEntriesByType("navigation")[0];
+
+    if (navigation && navigation.type === "reload") {
+        return;
+    }
+
+    const targetId = window.location.hash.substring(1);
+
+    if (targetId) {
+        const product = document.getElementById(targetId);
+
+        if (product && product.classList.contains("product")) {
+            product.classList.add("highlight");
+
+            setTimeout(() => {
+                product.classList.remove("highlight");
+            }, 3000);
+        }
+    }
+});
